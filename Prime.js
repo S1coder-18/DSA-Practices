d3d1376ffc5cf36559495b638e1d35e87c35c004
sachin Gupta <sachinguptasg7633780@gmail.com>
2025-04-22 12:57:30 +0530

    Prime Number Code

--- Prime.js
+++ Prime.js
@@ -1,5 +1,12 @@
-// Prime Number --> 
+// Prime Number --> is a number which should be divisible by either 1 or number itself.
+
+// If that number is divisible by any other number except 1 and that number itself then 
+// that is not a prime number.
+
+
 function Prime(number){
+
+    if(number < 1) return false;
     for(let i=2; i<number; i++){
         if(number%i === 0){
             return false;
@@ -8,5 +15,5 @@ function Prime(number){
     return true;
 }
 
-let number = 15;
+let number = 1;
 console.log(Prime(number));
