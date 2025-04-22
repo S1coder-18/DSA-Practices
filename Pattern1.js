98bbb9fa1b374941d5ec597dd59c8eecd57579a6
sachin Gupta <sachinguptasg7633780@gmail.com>
2025-04-22 13:30:43 +0530

    Pattern1 Priniting

--- Pattern1.js
+++ Pattern1.js
@@ -11,17 +11,22 @@
 function Pattern1(number){
     
     for(let i=0; i<number; i++){
+        
         for(let j=0; j<number; j++){
-            process.stdout.write('*');
+            process.stdout.write('*'); //It will print n number of times (*).
         }
-        console.log(); // It goes by default in new line.
+
+        console.log(); 
+
+        // It goes by default in new line. And this will start the inner loop to new line 
+        //whenever the inner loop condition fails. 
     }
 }
 
 let number = 4;
 Pattern1(number);
 
-//In js , if we will use this statement (process.stdout.write) then it won't go to in new line by default.
+//In js , if we will use this statement (process.stdout.write) then it won't go to new line by default.
 
 //Time Complexity --> O(n2) As we have used two for loops where inner loops runs n2 times.
 //Space Complexity --> O(1).
